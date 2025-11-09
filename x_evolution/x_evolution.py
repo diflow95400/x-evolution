35c6d48823661dce2db1142c2b9e8e8f5e0337dd
lucidrains <lucidrains@gmail.com>
2025-11-09 07:27:12 -0800

    neural network frameworks are not designed with evolution in mind, but that is ok

--- x_evolution/x_evolution.py
+++ x_evolution/x_evolution.py
@@ -1,2 +1,19 @@
+import torch
+from torch.nn import Module
+from torch.func import functional_call, vmap
 
 from x_mlps_pytorch.noisable import Noisable
+
+# helper functions
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+# class
+
+class Population(Module):
+    def __init__(self):
+        super().__init__()
